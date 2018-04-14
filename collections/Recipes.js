3ee3d12bd1cc8dbeb8f1d0c3366daa226bdaa181
Alan Tavares de Melo <alantmelo@live.com>
2018-04-14 12:28:43 -0300

    ingretient attributes

--- collections/Recipes.js
+++ collections/Recipes.js
@@ -1,5 +1,6 @@
 import SimpleSchema from 'simpl-schema';
 SimpleSchema.extendOptions(['autoform']);
+import { Mongo } from 'meteor/mongo';
 
 Recipes = new Mongo.Collection('recipes');
 
@@ -10,16 +11,54 @@ Recipes.allow({
     }
 });
 
+Ingretient = new SimpleSchema({
+    name:{
+        type: String
+    },
+    amount:{
+        type: String
+    }
+});
+
+
 RecipeSchema = new SimpleSchema({
     name: {
         type: String,
         label: "Name"
     },
 
+    inMenu:{
+        type: Boolean,
+        defaultValue: false,
+        optional: true,
+        autoform:{
+            type: 'hidden'
+        }
+    },
+
     desc: {
         type: String,
         label: "Descriptions"
     },
+
+    ingretient:{
+        type: Ingretient
+    },
+    // comments: {
+    //     type: [Object],
+    //     optional: true,
+    //     maxCount: 50
+    // },
+    // 'comments.$.userId': {
+    //     type: String
+    // },
+    // 'comments.$.date': {
+    //     type: Date
+    // },
+    // 'comments.$.text': {
+    //     type: String,
+    //     max: 500
+    // },
 
     author: {
         type: String,
